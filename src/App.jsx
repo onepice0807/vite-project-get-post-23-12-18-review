17e9b4e05384ef00b7a7f98fb817b9f667833fb6
onepice0807 <onepice0807@gmail.com>
2023-12-18 18:19:17 +0900

    2023-12-18 스타워즈 api를 이용해서 벡엔드와 연결 복습 1) App.jsx에서 fetch를 이용해서 스타워즈 api에서 영화 목록을 불러오도록 작성 2) API 요청 결과에 따라 로딩 상태, 에러 처리, 영화 목록 표시 기능 구현 3) Movie.jsx에서 영화항목을 표시하도록 작성 3) MoviesList.jsx에서 영화목록을 표시하도록 작성

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  async function fetchMoviesHandler() {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch("https://swapi.dev/api/films/");
+      if (!response.ok) {
+        throw new Error("문제가 발생했습니다!");
+      }
+      const data = await response.json();
+
+      const transformedMovies = data.results.map((movieDate) => {
+        return {
+          id: movieDate.episode_id,
+          title: movieDate.title,
+          openingText: movieDate.opening_crawl,
+          releaseDate: movieDate.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }
+  let content = <p>영화를 찾을수 없습니다</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>로딩중...!</p>;
+  }
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>{content}</section>
+    </React.Fragment>
+  );
+}
+
+export default App;
